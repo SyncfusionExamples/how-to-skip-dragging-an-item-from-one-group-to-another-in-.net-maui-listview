06a51255f1b6bfc2c9af1de485fbfdce927a647b
SowntharyaJ <sowntharya.jayamoorthy@syncfusion.com>
2025-04-22 13:26:49 +0530

    Changes done in App.xaml

--- ListViewMaui/App.xaml.cs
+++ ListViewMaui/App.xaml.cs
@@ -3,8 +3,11 @@ public partial class App : Application
 {
 	public App()
 	{
-        InitializeComponent();
+		InitializeComponent();
+	}
 
-        MainPage = new MainPage();
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage());
     }
 }
